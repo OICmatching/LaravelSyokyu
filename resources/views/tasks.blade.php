6424922faf25a3fc874b68fe32c99c518d5bac98
ymgn <gonyamagon@gmail.com>
2016-07-18 15:38:10 +0900

    少し修正

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -70,7 +70,7 @@
                                         {{ method_field('DELETE') }}
 
                                         <button type="submit" class="btn btn-danger">
-                                            <i class="fa fa-btn fa-trash"></i>Delete
+                                            <i class="fa fa-btn fa-trash"></i>タスク削除
                                         </button>
                                     </form>
                                 </td>
